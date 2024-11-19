8e6011a12b2b5a9e598b0474d91ae941fc121820
mostafa Gamal shaban <132886703+MostafaShaban2022@users.noreply.github.com>
2024-11-20 00:03:41 +0200

    Update ad_manager.dart

--- lib/services/ad_manager.dart
+++ lib/services/ad_manager.dart
@@ -1,5 +1,5 @@
 class AdManager {
-  static bool isTest = true;
+  static bool isTest = false;
 
   static String bannerHome = isTest
       ? 'ca-app-pub-3940256099942544/9214589741'
@@ -12,4 +12,16 @@ class AdManager {
   static String appOpenAd = isTest
       ? 'ca-app-pub-3940256099942544/9257395921'
       : 'ca-app-pub-6695947240389236/4231128358';
+
+  static String rewardedAd = isTest 
+  ? 'ca-app-pub-3940256099942544/5224354917' 
+  : 'ca-app-pub-6695947240389236/3610151566';
+
+  static String rewardedinterstitialAd = isTest 
+  ? 'ca-app-pub-3940256099942544/5354046379' 
+  : 'ca-app-pub-6695947240389236/8179951965';
+
+  static String nativeAd = isTest 
+  ? 'ca-app-pub-3940256099942544/2247696110' 
+  : 'ca-app-pub-6695947240389236/6334445886';
 }
